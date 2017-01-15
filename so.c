ead2593ce5e0a0cc3585432309ce3542a4725927
Diwakar kandel <damodarkandel435@gmail.com>
2017-01-15 10:44:59 +0545

    Add files via upload
    WAP to display the following output.

---
+++ so.c
@@ -0,0 +1,23 @@
+/*
+Name:
+subject:Diwakar Kandel
+Program:WAP to display the following output.
+Roll No:07
+Date:jan15,2017
+*/
+#include<stdio.h>
+int main()
+{
+  int i,j;
+ for(i=1;i<=5;i++)
+ {
+ for(j=1;j<=i;j++)
+ {
+
+
+ printf("*");
+ }
+ printf("\n");
+ }
+return 0;
+}
