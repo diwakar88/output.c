356507786a3eee8827dae530955cbe150b5d4d60
Diwakar kandel <damodarkandel435@gmail.com>
2017-01-15 10:17:14 +0545

    Add files via upload
    WAP to display the following output.

---
+++ output.c
@@ -0,0 +1,19 @@
+/*
+Name:
+subject:Diwakar Kandel
+Program:WAP to display the following output.
+Roll No:07
+Date:jan15,2017
+*/
+#include<stdio.h>
+int main()
+{
+ int i;
+char ch='A';
+ for(i=1;i<=7;i++)
+ {
+     printf("%c \t",ch);
+     ch++;
+}
+return 0;
+}
